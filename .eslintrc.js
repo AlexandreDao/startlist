dc5d996dcf3a937afb54e581a7d92a9206ecb507
alexandre <alexandre.dao@epitech.eu>
2021-04-29 03:29:57 +0900

    Fix unused variable error with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'prettier'],
   plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
   rules: {
     'prettier/prettier': ['error'],
     'react-native/no-inline-styles': 'off',
